8443251ee697b275020ebb7ac86171ef3ca45bfd
MaikHvz <maic.hernandez.dev@gmail.com>
2025-11-23 22:48:09 -0300

    funciona!!!!!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose.android)
 
+
+
     implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
     
     // ViewModel
@@ -61,12 +63,13 @@ dependencies {
     
     // Permissions
     implementation("com.google.accompanist:accompanist-permissions:0.32.0")
-    
-    // Networking (Retrofit + OkHttp + Gson)
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+// Networking (Retrofit + OkHttp + Gson)
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
